39e24fe51ce1c68b6399dd7dc56fc38f5dbeb835
Matt <mattmsj24@gmail.com>
2014-01-15 10:59:04 -0800

    Added my code

---
+++ MyCode/YouMaps/LoadProperties.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace YouMaps
+{
+    class LoadProperties
+    {
+        public LoadProperties()
+        {
+            //loadFolders();
+
+        }
+        private IReadOnlyList<StorageFolder> folders;
+        public async Task<IReadOnlyList<StorageFolder>> loadFolders()
+        {
+            int count;
+            StorageFolder rootFolder = KnownFolders.PicturesLibrary;
+            StorageFolder myfolder = await rootFolder.GetFolderAsync("YouMapsImages");
+            
+            folders = await myfolder.GetFoldersAsync();
+             count = folders.Count();
+           if(count == 0)
+           {
+               await myfolder.CreateFolderAsync("default", CreationCollisionOption.FailIfExists);
+               await myfolder.GetFoldersAsync();
+           }
+
+           return folders;
+            
+        }
+
+        public IReadOnlyList<StorageFolder> getFolders()
+        {
+            if (folders != null)
+            {
+                return folders;
+            }
+            else
+                return null;
+        }
+    }
+}
---
+++ MyCode/YouMaps/MainPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace YouMaps
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        LoadProperties lp; 
+        public MainPage()
+        {
+            lp = new LoadProperties();
+            this.InitializeComponent();
+            loadComboBox();
+
+            
+
+        }
+
+        private async void loadComboBox()
+        {
+
+            IReadOnlyList<StorageFolder> folders = await lp.loadFolders();
+            
+
+            if (folders != null)
+            {
+
+                ObservableCollection<string> folderName = new ObservableCollection<string>();
+                foreach(StorageFolder s in folders)
+                {
+                    folderName.Add(s.Name);
+                }
+                DropDownOfFolders.ItemsSource = folderName;
+                
+                
+            }
+            
+        }
+
+        private async void ImportImage(object sender, RoutedEventArgs e)
+        {
+            StorageFolder myfolder = await getMyRootfolder();
+            string folderName = DropDownOfFolders.SelectedValue.ToString();
+            StorageFolder selectedFolder = await myfolder.GetFolderAsync(folderName);
+            FileOpenPicker openfile = new FileOpenPicker();
+            openfile.ViewMode = PickerViewMode.List;
+            openfile.SuggestedStartLocation = PickerLocationId.Downloads;
+            openfile.FileTypeFilter.Add(".kml");
+            openfile.FileTypeFilter.Add(".jpeg");
+            openfile.FileTypeFilter.Add(".kmz");
+            openfile.FileTypeFilter.Add(".jpg");
+
+            StorageFile originalFile = await openfile.PickSingleFileAsync();
+            if(originalFile != null)
+            {
+                StorageFile filePathToCopy = await originalFile.CopyAsync(selectedFolder, originalFile.Name, NameCollisionOption.GenerateUniqueName);
+                Messages.Text = "File Import Done";
+            }
+                
+
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private async void CreateFolder(object sender, RoutedEventArgs e)
+        {
+            string newFoldPromt = "New folder name here.";
+            string noFolderEnteredMessage = "You must enter a new folder name";
+            if(!NewFolderNameTextBox.Text.Equals(newFoldPromt))
+            {
+                StorageFolder myfolder = await getMyRootfolder();
+                await myfolder.CreateFolderAsync(NewFolderNameTextBox.Text);
+                Messages.Text = "FolderCreated";
+                NewFolderNameTextBox.Text = newFoldPromt;
+                loadComboBox();
+            }
+            else
+            {
+                Messages.Text = noFolderEnteredMessage;
+            }
+        }
+
+        private static async Task<StorageFolder> getMyRootfolder()
+        {
+            StorageFolder rootFolder = KnownFolders.PicturesLibrary;
+            StorageFolder myfolder = await rootFolder.GetFolderAsync("YouMapsImages");
+            return myfolder;
+        }
+
+        private void GetCurrentLocation(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MapPage));
+          
+        }
+
+        private void GetImputedLocation(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
