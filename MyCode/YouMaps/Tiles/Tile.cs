86e4b5e6bd4bd143b3ee5eb19cba9d37f66be02c
Matt <mattmsj24@gmail.com>
2014-01-21 21:48:25 -0800

    Stuck at having to maybe change a key thing again

--- MyCode/YouMaps/Tiles/Tile.cs
+++ MyCode/YouMaps/Tiles/Tile.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace YouMaps.Tiles
 {
     class Tile
     {
+        public readonly int zoomLevel;
+        public readonly int xAxis;
+        public readonly int yAxis;
+        public readonly Image image = new Image();
+        public bool HasImageSource { get; private set; }
+
+
+        public Tile(int xAxis, int yAxis,int zoomLevel)
+        {
+            this.xAxis = xAxis;
+            this.yAxis = yAxis;
+            this.zoomLevel = zoomLevel;
+            
+        }
+
+        public ImageSource getImageSource()
+        {
+            return image.Source;
+        }
+
+        public void setImageSource(ImageSource image)
+        {
+            this.image.Source = image;
+            HasImageSource = true;
+        }
+
+        public int Xindex
+        {
+            get
+            {
+                var numberOfTiles = 1 << zoomLevel;
+                return ((xAxis % numberOfTiles) + numberOfTiles) % numberOfTiles;
+                 
+            }
+        }
+
+
+
+
+
+
+
     }
 }
--- MyCode/YouMaps/Tiles/TileDownloader.cs
+++ MyCode/YouMaps/Tiles/TileDownloader.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace YouMaps.Tiles
 {
-    class Tileloader
+    class TileLoader
     {
+        public const string defaultCasheName = "YouMapsTileCashe";
+     //   public static string defaultCasheDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "YouMaps");
+        public static string defaultCasheDirectory = "here for now";
+
+        private ConcurrentQueue<Tile> tilesInQue = new ConcurrentQueue<Tile>();
+        private int numOfThreads;
+
+        protected void StartGetTiles(TileLayer tileLayer, IEnumerable<Tile> tiles)
+        {
+            
+        }
 
     }
 }
