c43ed3f38f9a14576bf1ec45bb0d8da928fb74a9
Matt <mattmsj24@gmail.com>
2014-01-26 23:08:06 -0700

    more stuff added

---
+++ MyCode/YouMaps/LongLatTransform.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace YouMaps
+{
+    class LongLatTransform
+    {
+        private static readonly double maxLatitude = Math.Atan(Math.Sinh(Math.PI)) / Math.PI * 180d;
+
+        public override double MaxLatitude
+        {
+            get { return maxLatitude; }
+        }
+
+        public override double RelativeScale(Location location)
+        {
+            if (location.Latitude <= -90d)
+            {
+                return double.NegativeInfinity;
+            }
+
+            if (location.Latitude >= 90d)
+            {
+                return double.PositiveInfinity;
+            }
+
+            return 1d / Math.Cos(location.Latitude * Math.PI / 180d);
+        }
+
+        public override Point Transform(Location location)
+        {
+            double latitude;
+
+            if (location.Latitude <= -90d)
+            {
+                latitude = double.NegativeInfinity;
+            }
+            else if (location.Latitude >= 90d)
+            {
+                latitude = double.PositiveInfinity;
+            }
+            else
+            {
+                latitude = location.Latitude * Math.PI / 180d;
+                latitude = Math.Log(Math.Tan(latitude) + 1d / Math.Cos(latitude)) / Math.PI * 180d;
+            }
+
+            return new Point(location.Longitude, latitude);
+        }
+
+        public override Location Transform(Point point)
+        {
+            var latitude = Math.Atan(Math.Sinh(point.Y * Math.PI / 180d)) / Math.PI * 180d;
+
+            return new Location(latitude, point.X);
+        }
+    }
+    }
+}
--- MyCode/YouMaps/Tiles/TileLayer.cs
+++ MyCode/YouMaps/Tiles/TileLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Windows.Foundation;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
@@ -12,9 +13,30 @@ namespace YouMaps.Tiles
         
         private readonly MatrixTransform transform = new MatrixTransform();
         private List<Tile> tiles = new List<Tile>();
+        private TRectangle grid;
+        private int zoomLevel;
 
-        
-            
+        public static TileLayer Default
+        {
+            get
+            {
+                return new TileLayer
+                {
+                    SourceName = "OpenStreetMap",
+                    Description = "© {y} OpenStreetMap Contributors, CC-BY-SA",
+                    TileSource = new TileSource("http://{c}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+                };
+            }
+        }
+        public string SourceName { get; set; }
+        public TileSource TileSource { get; set; }
+
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value.Replace("{y}", DateTime.Now.Year.ToString()); }
+        }
         public void Initialize()
         {
             RenderTransform = transform;
@@ -29,10 +51,33 @@ namespace YouMaps.Tiles
         protected void RenderTiles()
         {
             Children.Clear();
-            foreach(Tile tile in tiles);
+            foreach(var tile in tiles)
             {
-                Children.Add(tile.Image);
+                
+                Children.Add(tile.image);
             }
+        }
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (var tile in tiles)
+            {
+                tile.image.Measure(availableSize);
+            }
+
+            return new Size();
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach(var tile in tiles)
+            {
+                var tileSoze = (double)(256 << zoomLevel - tile.zoomLevel);
+                tile.image.Width = tileSoze;
+                tile.image.Height = tileSoze;
+                tile.image.Arrange(new Rect(tileSoze * tile.xAxis - 256, tileSoze * tile.yAxis - 256 * grid.Y, tileSoze, tileSoze));
+
+            }
+            return finalSize;
         }
     }
     }
